65411b983342dec0297c14df872fdc3736ff6d76
caiyejiao <964037663@qq.com>
2020-11-30 21:41:00 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,29 @@
-const path = require('path')
+const path = require("path");
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 module.exports = {
+  publicPath: "/my_project/",
   configureWebpack: {
-    publicPath:'/my_project/',
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
     resolve: {
       alias: {
-        imgs: resolve('src/assets/imgs'),
-        '@': resolve('src')
-      }
-    }
+        imgs: resolve("src/assets/imgs"),
+        "@": resolve("src"),
+      },
+    },
   },
   pwa: {
     iconPaths: {
-      favicon32: 'favicon.ico',
-      favicon16: 'favicon.ico',
-      appleTouchIcon: 'favicon.ico',
-      maskIcon: 'favicon.ico',
-      msTileImage: 'favicon.ico'
-    }
-  }
+      favicon32: "favicon.ico",
+      favicon16: "favicon.ico",
+      appleTouchIcon: "favicon.ico",
+      maskIcon: "favicon.ico",
+      msTileImage: "favicon.ico",
+    },
+  },
   // ,
   // module: {
   //   rules:[
@@ -36,5 +36,4 @@ module.exports = {
   //       }
   //   }]
   // },
-  
-}
+};
