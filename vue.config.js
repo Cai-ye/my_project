8fe1870f3e3d3d55a15708ef9f6582e39e08e6be
Cai-ye <964037663@qq.com>
2020-11-30 14:20:12 +0800

    修改路径

--- vue.config.js
+++ vue.config.js
@@ -29,8 +29,8 @@ module.exports = {
       test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
       loader: 'url-loader',
       options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          limit: 10000
+          // name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
       }
   }]
 }
