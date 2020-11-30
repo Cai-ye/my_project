22c24b8d270c4474a62d256260726f26f861e560
caiyejiao <964037663@qq.com>
2020-11-30 22:47:01 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false;
-
+import { Carousel, CarouselItem } from "element-ui";
+Vue.use(Carousel);
+Vue.use(CarouselItem);
 new Vue({
   router,
   render: (h) => h(App),
--- src/pages/Display.vue
+++ src/pages/Display.vue
@@ -1,6 +1,55 @@
 <template>
-  <div>11</div>
+  <div class="page">
+    <div class="left">
+      <div class="title">111</div>
+      <div class="sub-title">222</div>
+      <div class="content">
+        333
+      </div>
+    </div>
+    <div class="right">
+      <el-carousel
+        trigger="click"
+        height="550px"
+        indicator-position="none"
+        autoplay="false"
+      >
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
+<style>
+.page {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+}
+.left {
+  max-width: 300px;
+  width: 100%;
+}
+.right {
+  flex: 1;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 500px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
