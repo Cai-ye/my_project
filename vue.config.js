323d3dedc369e2a2f172400436a97dc657309c05
Cai-ye <964037663@qq.com>
2020-11-30 14:27:38 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -24,13 +24,16 @@ module.exports = {
       msTileImage: 'favicon.ico'
     }
   },
-  rules:[
-    {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-          limit: 10000
-          // name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-      }
-  }]
+  module: {
+    rules:[
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+            limit: 10000
+            // name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+    }]
+  },
+  
 }
