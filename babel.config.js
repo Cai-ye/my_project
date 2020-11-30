d9e22603fc584a3384a035ac5c0637fd3d71a776
Cai-ye <964037663@qq.com>
2020-11-30 14:07:35 +0800

    更新

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  rules:[
-    {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-      }
-  }
   ]
+
 }
